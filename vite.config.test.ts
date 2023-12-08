592a13c6f9fa333c545e6aa3d7f356d21fa86e2b
“thiagotcs” <thiagotcs.dev@gmail.com>
2023-12-07 22:58:11 -0300

    chore: update dependencies

---
+++ vite.config.test.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+
+export default {
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    testMatch: ['./tests/**/*.test.tsx'],
+    globals: true,
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config: UserConfig = {
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['@testing-library/jest-dom'],
+  },
+}
+
+export default config
