bed58562db626067af94e5a1c41ef57cd8f598d5
“thiagotcs” <thiagotcs.dev@gmail.com>
2023-12-07 23:01:26 -0300

    refactor: improvement Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../store'
+import { RootState, useAppDispatch, useAppSelector } from '../../store'
 import { loadProducers } from '../../store/producersSlice'
 import { Button } from '../Button'
 import { DashboardBox } from './DashboardBox'
 import { PieChart } from '../PieChart'
+import { Loader } from 'lucide-react'
 
 export const Dashboard = () => {
   const dispatch = useAppDispatch()
-  const { producers } = useAppSelector((state) => state.producers)
+  const { producers } = useAppSelector((state: RootState) => state.producers)
   useEffect(() => {
     dispatch(loadProducers())
   }, [dispatch])
@@ -39,8 +40,8 @@ export const Dashboard = () => {
     },
   ]
 
-  return (
-    <div className="mt-6 flex flex-col ">
+  return producers.length > 0 ? (
+    <div className="mt-6 flex flex-col" data-testid="data-element-id">
       <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
         <div className="space-y-1">
           <h2 className="text-lg font-medium text-zinc-900">Dashboard</h2>
@@ -53,7 +54,10 @@ export const Dashboard = () => {
         </div>
       </div>
       <DashboardBox producers={producers} />
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+      <div
+        data-testid="chart-test-id"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3"
+      >
         {chartsData.map((chart, index) => (
           <PieChart
             key={index}
@@ -63,6 +67,13 @@ export const Dashboard = () => {
           />
         ))}
       </div>
+    </div>
+  ) : (
+    <div className="flex h-full items-center justify-center">
+      <Loader
+        className="h-6 w-6 animate-spin text-zinc-400"
+        data-testid="loading-indicator"
+      />
     </div>
   )
 }
